3ace7aedcd40a72b8018621238f51585eb9e80d5
Ayush Pattanayak <53310507+ayush-sys@users.noreply.github.com>
2021-01-24 20:04:38 +0530

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,49 @@
+# This is a Random Password & OTP Generator written in Python
+
+# Import Library
+import math, random, string
+
+
+def generate_OTP(len) :
+    
+	digits = "0123456789"
+	OTP = "" 
+
+	for i in range(len) : 
+		OTP += digits[math.floor(random.random() * 10)] 
+
+	return OTP 
+
+
+def generate_password(len) :
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for i in range(len)))
+    return result_str
+
+
+def Main() :
+    print("!! Random Password Generator !!")
+    print("1: Generate Password")
+    print("2: Generate OTP")
+    choice = int(input("Enter your choice : "))
+
+    if choice == 0 :
+        exit()
+
+    elif choice == 1:
+        len = int(input("Enter the length of Password : "))
+        print("password : ",generate_password(len))
+
+    elif choice ==2:
+        len = int(input("Enter the length of OTP : "))
+        print("OTP : ",generate_OTP(len))
+
+    else :
+        print("OOPS !!")
+        print("Check your input.")
+
+    
+
+
+if __name__ == "__main__" : 
+    Main()
